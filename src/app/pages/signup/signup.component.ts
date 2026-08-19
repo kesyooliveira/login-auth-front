f842d13aa5d0c2f7b3b7eb93fe7374ae6d942132
Kesyo <kesyooliveira@gmail.com>
2026-08-18 21:43:43 -0300

    ferat: creating signup page and fix toastr in login page

--- src/app/pages/signup/signup.component.ts
+++ src/app/pages/signup/signup.component.ts
@@ -6,41 +6,45 @@ import { Router } from '@angular/router';
 import { LoginService } from '../../services/login.service';
 import { ToastrService } from 'ngx-toastr';
 
-interface LoginForm {
-    email: FormControl,
-    password: FormControl
+interface SignupForm {
+  name: FormControl,
+  email: FormControl,
+  password: FormControl,
+  passwordConfirm: FormControl
 }
 
 @Component({
-    selector: 'app-login',
+    selector: 'app-signup',
     standalone: true,
     imports: [DefaultLoginLayoutComponent, ReactiveFormsModule, PrimaryInputComponent],
     providers: [LoginService],
-    templateUrl: './login.component.html',
-    styleUrl: './login.component.scss',
+    templateUrl: './signup.component.html',
+    styleUrl: './signup.component.scss',
 })
-export class LoginComponent {
-    loginForm!: FormGroup<LoginForm>;
+export class SignupComponent {
+    signupForm!: FormGroup<SignupForm>;
 
     constructor(
         private router: Router,
         private loginService: LoginService,
         private toastService: ToastrService
     ) {
-        this.loginForm = new FormGroup({
+        this.signupForm = new FormGroup({
+            name: new FormControl('', [Validators.required, Validators.minLength(3)]),
             email: new FormControl('', [Validators.required, Validators.email]),
-            password: new FormControl('', [Validators.required, Validators.minLength(6)])
+            password: new FormControl('', [Validators.required, Validators.minLength(6)]),
+            passwordConfirm: new FormControl('', [Validators.required, Validators.minLength(6)])
         });
     }
 
     submit() {
-        this.loginService.login(this.loginForm.value.email, this.loginForm.value.password).subscribe({
-            next: () => this.toastService.success('Successfully logged in!'),
+        this.loginService.login(this.signupForm.value.email, this.signupForm.value.password).subscribe({
+            next: () => this.toastService.success('Successfully signed up!'),
             error: () => this.toastService.error('Woops, something went wrong, try again later!')
         });
     }
 
     navigate() {
-        this.router.navigate(['signup']);
+        this.router.navigate(['login']);
     }
 }
